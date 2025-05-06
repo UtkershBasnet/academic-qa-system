e34a01614549b739de46868324c6c1aec3b9405b
UtkershBasnet <utkershbasnet@gmail.com>
2025-05-06 12:04:16 +0530

    changes made to deploy

--- server/server.js
+++ server/server.js
@@ -9,6 +9,13 @@ const { GoogleGenerativeAI } = require("@google/generative-ai")
 const app = express()
 const PORT = process.env.PORT || 5000
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+  });
+  
+}
 // Middleware
 app.use(cors())
 app.use(bodyParser.json())
